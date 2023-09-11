4a43e1970bbaddd0abd880ab29e3a63a22c54d4e
nauxscript <nauxscript@gmail.com>
2023-09-11 10:53:03 +0800

    fix: url match rules

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Swagger typescript helper
 // @license      MIT
-// @version      0.0.2
+// @version      0.0.3
 // @description  一个帮助用户在 Swagger UI 中快速生成对应 Typescript 接口的脚本 / A script that assists users in quickly generating corresponding TypeScript interfaces in Swagger UI.
 // @author       Nauxscript
 // @run-at       document-start
+// @match        */swagger-ui/*
 // @match        */*/swagger-ui/*
+// @match        */*/*/swagger-ui/*
 // @namespace    Nauxscript
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.js
 // ==/UserScript==
