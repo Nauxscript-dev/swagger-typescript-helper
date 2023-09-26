14a50f9359467aab1d8d605623d7c247c41f709e
nauxscript <nauxscript@gmail.com>
2023-09-26 15:23:05 +0800

    chore: release v0.0.6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Swagger typescript helper
 // @license      MIT
-// @version      0.0.5
+// @version      0.0.6
 // @description  一个帮助用户在 Swagger UI 中快速生成对应 Typescript 接口的脚本 / A script that assists users in quickly generating corresponding TypeScript interfaces in Swagger UI.
 // @author       Nauxscript
 // @run-at       document-start
